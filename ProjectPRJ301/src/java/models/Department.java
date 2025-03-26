2ec91ddb75eb2a150d3a82fd73c1bb0996659e15
An23-09 <anpthe181914@fpt.edu.vn>
2025-03-26 14:34:38 +0700

    Update Department.java

--- ProjectPRJ301/src/java/models/Department.java
+++ ProjectPRJ301/src/java/models/Department.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package models;
 
-/**
- *
- * @author PHAM THAI AN
- */
 public class Department {
     private String depId;
     private String depName;
